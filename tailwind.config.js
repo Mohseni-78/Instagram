1b626f20e81ffcd157502a2f4b8d80418b264c40
Alireza Mohseni <mohseni.arz@gmail.com>
2023-03-31 01:55:14 +0430

    initial implement and clean app for coding also install packages it use

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*/{ts,js,tsx,jsx}",
+    "./components/**/*/{ts,js,tsx,jsx}",
+  ],
+  theme: {
+    fontFamily:{
+      Montserrat :['Montserrat']
+    },
+    extend: {},
+  },
+  plugins: [],
+};
